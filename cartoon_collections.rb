12a9dfa0a7a4706385ae97d18a7c0c7e866dd5d0
Isabella <isabella.y.hu@gmail.com>
2022-08-08 21:14:59 +1000

    commit tree aa841f47b8dcff41979fc2757151d1e7cce99e63

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,20 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(dwarves) # code an argument here
   # Your code here
+  dwarves.each.with_index(1) {|dwarf,index| puts "#{index}. #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(calls)# code an argument here
   # Your code here
+  calls.map {|w| "#{w.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(calls)# code an argument here
   # Your code here
+  calls.any?{|w| w.length > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(foods)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.detect {|cheese| foods.include?(cheese)}
 end
